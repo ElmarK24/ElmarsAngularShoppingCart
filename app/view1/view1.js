83641641a0b7e313eb4f4e828f6ad3f69bfa2fff
Robert <cfrt2006@yahoo.com>
2017-04-29 23:05:30 -0400

    latest

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,4 +1,93 @@
 'use strict';
+(function(global){
+	
+	global.ElmarLib = function(){}
+    var orderIdInc = 0;
+
+
+    ElmarLib.addProductAndLineItemToOrder = addProductAndLineItemToOrder;
+
+    ElmarLib.getNewOrder = getNewOrder;
+
+    function getNewOrder()
+    {
+
+
+        var ret = {
+            id: orderIdInc,
+            LineItems: [],
+            SubTotal: 0,
+            Taxes: 0,
+            Total: 0
+        }
+
+        orderIdInc ++
+
+        return ret;
+    }
+
+    function getNewLineItem()
+    {
+        return {
+            CreatedDate: new Date(),
+            Product: undefined,
+            Quantity: 0,
+            SubTotal: 0,
+            Taxes: 0,
+            Total: 0
+        }
+    }
+
+
+	function addProductAndLineItemToOrder(Order, Product, Quantity)
+	{
+		Quantity = Quantity || 1;
+
+        if(typeof Quantity !== 'number')
+        {
+            Quantity = parseInt(Quantity) || 1
+        }
+
+        var LineItemToAddTo
+
+        for(var i = 0; i < Order.LineItems.length; i++)
+        {
+            if(Order.LineItems[i].Product.id == Product.id)
+            {
+                LineItemToAddTo = Order.LineItems[i]
+            }
+        }
+
+        if(!LineItemToAddTo)
+        {
+            LineItemToAddTo =  getNewLineItem();
+            LineItemToAddTo.Product = Product
+
+            LineItemToAddTo.Quantity = 0;
+
+            Order.LineItems.push(LineItemToAddTo);
+        }
+
+        LineItemToAddTo. Quantity += Quantity
+
+
+
+        LineItem.Total =  Quantity * Product.Price
+
+		
+		// Recalculate SubTotal, Tax and Total
+	}
+
+	function recalculateTotal(Order)
+    {
+
+    }
+	
+	
+})(window)
+
+
+
 
 var app = angular.module('myApp.view1', ['ngRoute'])
 
@@ -11,28 +100,32 @@ var app = angular.module('myApp.view1', ['ngRoute'])
 
 app.controller('View1Ctrl', function($scope) {
 
-    $scope.orders = [];
+    $scope.Order = ElmarLib.getNewOrder();
+
     $scope.catalogOfProducts = [
       {
+          id: 0,
           name: "Apple",
           price: 1.00,
-          quantity: 0,
+
           taxRate: 0.13,
           image: "images/apple.jpg",
           maxQuantityPerOrder: 5
       },
       {
+          id: 1,
+
           name: "Orange",
           price: 1.25,
-          quantity: 0,
           taxRate: 0.13,
           image: "images/orange.jpg",
           maxQuantityPerOrder: 5
       },
       {
+          id: 2,
+
           name: "Banana",
           price: 0.75,
-          quantity: 0,
           taxRate: 0.13,
           image: "images/banana.jpg",
           maxQuantityPerOrder: 5
@@ -40,37 +133,45 @@ app.controller('View1Ctrl', function($scope) {
   ];
 
 
-    $scope.addItem = function(product)
+
+
+
+
+    $scope.addItem = function(order, product, quantity)
     {
-        if(product){
-            if(product.quantity>0 && product.quantity<=5) {
-                $scope.orders.push({
-                    name: product.name, price: product.price, quantity: product.quantity, taxRate: product.taxRate,
-                    maxQuantityPerOrder: product.maxQuantityPerOrder
-                });
-            }
-            if(product.quantity>product.maxQuantityPerOrder){
-                alert("You cannot order more than "+product.maxQuantityPerOrder +" "+ product.name +"s");
-            }
-            if(product.quantity<0){
-                alert("You need to add a quantity");
-            }
-        }
+        debugger;
+        ElmarLib.addProductAndLineItemToOrder(order, product, quantity)
+
+
+        // if(product){
+        //     if(product.quantity>0 && product.quantity<=5) {
+        //         $scope.orders.push({
+        //             name: product.name, price: product.price, quantity: product.quantity, taxRate: product.taxRate,
+        //             maxQuantityPerOrder: product.maxQuantityPerOrder
+        //         });
+        //     }
+        //     if(product.quantity>product.maxQuantityPerOrder){
+        //         alert("You cannot order more than "+product.maxQuantityPerOrder +" "+ product.name +"s");
+        //     }
+        //     if(product.quantity<0){
+        //         alert("You need to add a quantity");
+        //     }
+        // }
     }
 
     $scope.totalPrice = 0;
 
     $scope.setTotalPrice = function(order){
-        if(order){
-            $scope.totalPrice += (((order.price*order.taxRate)+order.price)*order.quantity);}
+        // if(order){
+        //     $scope.totalPrice += (((order.price*order.taxRate)+order.price)*order.quantity);}
 
     }
 
     $scope.removeItem = function(order){
-        if(order){
-            $scope.orders.splice($scope.orders.indexOf(order), 1);
-            $scope.totalPrice -= (((order.price*order.taxRate)+order.price)*order.quantity);
-        }
+        // if(order){
+        //     $scope.orders.splice($scope.orders.indexOf(order), 1);
+        //     $scope.totalPrice -= (((order.price*order.taxRate)+order.price)*order.quantity);
+        // }
     }
 
 });
